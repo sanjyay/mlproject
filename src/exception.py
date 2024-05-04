d0b7631a3dd22e250a05760fefa9584563600d06
sanjyay <sanjay277@zohomail.in>
2024-05-04 17:20:29 +0530

    enable logging of exceptions

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+from src.logger import logging
+
 
 def error_message_detail(error,error_detail:sys):
     
@@ -17,5 +19,3 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-
-
